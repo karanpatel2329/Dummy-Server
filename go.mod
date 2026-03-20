cbd56bae868fc4091bc39f39268bf06dbbdf1723
karanpatel2329 <karanpatel2329@gmail.com>
2026-03-20 07:09:15 +0530

    download pdf endpoint added

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.25.0
 
 require (
 	github.com/andybalholm/brotli v1.2.0 // indirect
+	github.com/go-pdf/fpdf v0.9.0 // indirect
 	github.com/gofiber/fiber/v3 v3.1.0 // indirect
 	github.com/gofiber/schema v1.7.0 // indirect
 	github.com/gofiber/utils/v2 v2.0.2 // indirect
--- main.go
+++ main.go
@@ -2,9 +2,28 @@ package main
 
 import (
 	"os"
+	"strconv"
 
+	"github.com/go-pdf/fpdf"
 	"github.com/gofiber/fiber/v3"
 )
+
+type UserBalance struct {
+	Name string `json:"name"`
+	Age  int    `json:"age"`
+}
+
+func generatePdf(u UserBalance) {
+	pdf := fpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, u.Name)
+	pdf.Cell(80, 10, strconv.Itoa(u.Age))
+	err := pdf.OutputFileAndClose("hello.pdf")
+	if err != nil {
+		panic(err)
+	}
+}
 
 func main() {
 	app := fiber.New()
@@ -13,8 +32,19 @@ func main() {
 		return c.SendString("Hello, World!")
 	})
 
-	app.Get("/download", func(c fiber.Ctx) error {
-		return c.Res().Download("test.pdf")
+	app.Post("/download", func(c fiber.Ctx) error {
+		var user UserBalance
+		if err := c.Bind().Body(&user); err != nil {
+			return c.Status(400).SendString("Invalid JSON")
+		}
+		if user.Name == "" || user.Age == 0 {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Name and Amount are required",
+			})
+		}
+		generatePdf(user)
+
+		return c.Res().Download("hello.pdf")
 	})
 
 	app.Listen(":" + port)
